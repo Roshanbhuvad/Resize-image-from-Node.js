df1ece06dbe1f81007a0a56a28fdc9e643d4bc9b
Roshan <roshanbhuvad15@gmail.com>
2020-09-15 09:32:16 +0530

    Working on Resizing the images

---
+++ resize.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+module.exports = function resize(path, format, width,height) {
+	const readStream = fs.createReadStream(path)
+
+	return readStream;
+}
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const server = express();
+const resize = require("./resize")
 
+server.get("/", (req, res) => {
+ res.type("image/jpg");
+ resize("image1.jpg").pipe(res);
+}) 
 server.listen(5000, () => {
 	console.log("Server started")
 })
