60e212f6bef5c33f81afc3ad20388eea74caf80f
Roshan <roshanbhuvad15@gmail.com>
2020-09-15 09:47:47 +0530

    Query params added

--- resize.js
+++ resize.js
@@ -1,7 +1,18 @@
 const fs = require("fs");
+const sharp = require("sharp");
+
 
 module.exports = function resize(path, format, width,height) {
 	const readStream = fs.createReadStream(path)
+	let transform = sharp();
 
-	return readStream;
+if(format) {
+	transform = transform.toFormat(format);
+}
+
+if(width || height) {
+	transform = transform.resize(width, height)
+}
+return readStream.pipe(transform)
+
 }
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require("express");
-const server = express();
 const resize = require("./resize")
+const server = express();
+
 
 server.get("/", (req, res) => {
- res.type("image/jpg");
- resize("image1.jpg").pipe(res);
+
+	// Extract the query-parameter 
+	const widthString = req.query.width
+	const heightString = req.query.height
+	const format = req.query.format
+
+	// Parse to integer if possible
+	let width, height;
+	if(widthString) {
+		width = parseInt(widthString)
+	}
+	if(heightString) {
+		height = parseInt(heightString)
+	}
+
+	//  set the content-type of the response
+ res.type(`image/${format} || "jpg"}`)
+
+  //http://localhost:5000/?format=jpg&width=200&height=200
+ 
+ 	// Get the resized image
+ resize("image1.jpg",format, width,height).pipe(res);
 }) 
+
 server.listen(5000, () => {
 	console.log("Server started")
 })
