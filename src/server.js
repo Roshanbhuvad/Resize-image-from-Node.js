588b3e883917d0d3857c179efcc0e86f138924ba
Roshan <roshanbhuvad15@gmail.com>
2020-09-15 13:20:57 +0530

    Netlify and serverless-http added

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const resize = require("./resize")
-const server = express();
+const serverless = require("serverless-http");
+
+const app = express();
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
 
 
-server.get("/", (req, res) => {
-
-	// Extract the query-parameter 
 	const widthString = req.query.width
 	const heightString = req.query.height
 	const format = req.query.format
 
-	// Parse to integer if possible
+
 	let width, height;
 	if(widthString) {
 		width = parseInt(widthString)
@@ -19,15 +22,15 @@ server.get("/", (req, res) => {
 		height = parseInt(heightString)
 	}
 
-	//  set the content-type of the response
  res.type(`image/${format} || "jpg"}`)
 
-  //http://localhost:5000/?format=jpg&width=200&height=200
  
- 	// Get the resized image
  resize("image1.jpg",format, width,height).pipe(res);
 }) 
 
-server.listen(5000, () => {
-	console.log("Server started")
-})
+
+
+app.use(`/.netlify/functions/server`,router);
+
+
+module.exports.handler = serverless(app);
